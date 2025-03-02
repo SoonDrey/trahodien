fefd061fba5db6ed94a2bb1c232fab1b267a0d5c
SoonDrey <126157488+SoonDrey@users.noreply.github.com>
2025-03-02 13:31:02 +0700

    first commit

---
+++ js/data.js
@@ -0,0 +1,753 @@
+const jsonData = {
+    data:[
+    {
+        "STT": "1",
+        "Họ và Tên": "Huỳnh Minh Phúc",
+        "Tên khác": "Đào gấm",
+        "Mã Số Điện": "32040",
+        "Số điện thoại": "0973715446"
+    },
+    {
+        "STT": "2",
+        "Họ và Tên": "Lê Thị Bé Ba",
+        "Tên khác": "",
+        "Mã Số Điện": "34497",
+        "Số điện thoại": "0346664007"
+    },
+    {
+        "STT": "3",
+        "Họ và Tên": "Nguyễn Minh Cảnh",
+        "Tên khác": "Cảnh 7 Tiên",
+        "Mã Số Điện": "06182",
+        "Số điện thoại": "0857003261"
+    },
+    {
+        "STT": "4",
+        "Họ và Tên": "Đỗ Văn Riết",
+        "Tên khác": "5 Ri",
+        "Mã Số Điện": "31832",
+        "Số điện thoại": "0783286152"
+    },
+    {
+        "STT": "5",
+        "Họ và Tên": "Đỗ Văn Riết",
+        "Tên khác": "5 Ri Tưới",
+        "Mã Số Điện": "44877",
+        "Số điện thoại": "0783286152"
+    },
+    {
+        "STT": "6",
+        "Họ và Tên": "Huỳnh Văn Lấn",
+        "Tên khác": "7 Lấn",
+        "Mã Số Điện": "06211",
+        "Số điện thoại": "0334100723"
+    },
+    {
+        "STT": "7",
+        "Họ và Tên": "Trần Thị Tồn",
+        "Tên khác": "",
+        "Mã Số Điện": "13744",
+        "Số điện thoại": "0369371141"
+    },
+    {
+        "STT": "8",
+        "Họ và Tên": "Toàn út nhỏ",
+        "Tên khác": "",
+        "Mã Số Điện": "50466",
+        "Số điện thoại": "0369371141"
+    },
+    {
+        "STT": "9",
+        "Họ và Tên": "Phạm Công Tới",
+        "Tên khác": "Tiến 5 Tình",
+        "Mã Số Điện": "31822",
+        "Số điện thoại": "0392842417"
+    },
+    {
+        "STT": "10",
+        "Họ và Tên": "Đổ Hoàng Phương",
+        "Tên khác": "5 Ri",
+        "Mã Số Điện": "33691",
+        "Số điện thoại": "0941889527"
+    },
+    {
+        "STT": "11",
+        "Họ và Tên": "Phạm Thu Hòa",
+        "Tên khác": "",
+        "Mã Số Điện": "06199",
+        "Số điện thoại": "0983768270"
+    },
+    {
+        "STT": "12",
+        "Họ và Tên": "Phạm Ngọc Hiệp",
+        "Tên khác": "",
+        "Mã Số Điện": "44167",
+        "Số điện thoại": "0977787472"
+    },
+    {
+        "STT": "13",
+        "Họ và Tên": "Lê Thị Bé",
+        "Tên khác": "6 Về",
+        "Mã Số Điện": "06222",
+        "Số điện thoại": "0355800174"
+    },
+    {
+        "STT": "14",
+        "Họ và Tên": "Nguyễn Hoàng Phương",
+        "Tên khác": "Phương 2 Cự",
+        "Mã Số Điện": "31739",
+        "Số điện thoại": "0385231661"
+    },
+    {
+        "STT": "15",
+        "Họ và Tên": "Kim Vỉnh Thuận",
+        "Tên khác": "Phương 2 Cự",
+        "Mã Số Điện": "31712",
+        "Số điện thoại": "0385231661"
+    },
+    {
+        "STT": "16",
+        "Họ và Tên": "Tú",
+        "Tên khác": "",
+        "Mã Số Điện": "21382",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "17",
+        "Họ và Tên": "Nguyễn Phước Châu",
+        "Tên khác": "Gấm",
+        "Mã Số Điện": "29503",
+        "Số điện thoại": "0977732665"
+    },
+    {
+        "STT": "18",
+        "Họ và Tên": "Trần Thanh Giỏi",
+        "Tên khác": "6 Bờ",
+        "Mã Số Điện": "48206",
+        "Số điện thoại": "0839553527"
+    },
+    {
+        "STT": "19",
+        "Họ và Tên": "Nguyễn Thị Dung",
+        "Tên khác": "Giỏi",
+        "Mã Số Điện": "06185",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "20",
+        "Họ và Tên": "Trần Văn Ý",
+        "Tên khác": "",
+        "Mã Số Điện": "06214",
+        "Số điện thoại": "0974485025"
+    },
+    {
+        "STT": "21",
+        "Họ và Tên": "Vỏ Thanh Triêm",
+        "Tên khác": "",
+        "Mã Số Điện": "15134",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "22",
+        "Họ và Tên": "Nguyễn Hoàn Danh",
+        "Tên khác": "Đèo",
+        "Mã Số Điện": "06195",
+        "Số điện thoại": "0977753541"
+    },
+    {
+        "STT": "23",
+        "Họ và Tên": "Võ Trung Lập",
+        "Tên khác": "",
+        "Mã Số Điện": "06213",
+        "Số điện thoại": "0392192056"
+    },
+    {
+        "STT": "24",
+        "Họ và Tên": "Hồ Nhan Nhựt",
+        "Tên khác": "Dũng Nhựt",
+        "Mã Số Điện": "21621",
+        "Số điện thoại": "0976772820"
+    },
+    {
+        "STT": "25",
+        "Họ và Tên": "Võ Văn Y",
+        "Tên khác": "",
+        "Mã Số Điện": "06187",
+        "Số điện thoại": "0977309057"
+    },
+    {
+        "STT": "26",
+        "Họ và Tên": "Võ Thị Âu",
+        "Tên khác": "",
+        "Mã Số Điện": "26173",
+        "Số điện thoại": "0386222218"
+    },
+    {
+        "STT": "27",
+        "Họ và Tên": "Nguyễn Hoàng Thu",
+        "Tên khác": "Vẹn",
+        "Mã Số Điện": "06206",
+        "Số điện thoại": "0327388125"
+    },
+    {
+        "STT": "28",
+        "Họ và Tên": "Phạm Trung Thành",
+        "Tên khác": "Ân",
+        "Mã Số Điện": "23760",
+        "Số điện thoại": "0335199925"
+    },
+    {
+        "STT": "29",
+        "Họ và Tên": "Phạm Thành Tâm ",
+        "Tên khác": "Khoáng",
+        "Mã Số Điện": "31811",
+        "Số điện thoại": "0976561161"
+    },
+    {
+        "STT": "30",
+        "Họ và Tên": "Danh Mộng Hùng",
+        "Tên khác": "Nhản",
+        "Mã Số Điện": "21752",
+        "Số điện thoại": "0947172990"
+    },
+    {
+        "STT": "31",
+        "Họ và Tên": "Phạm Thị Thu Yên",
+        "Tên khác": "Mực",
+        "Mã Số Điện": "31833",
+        "Số điện thoại": "0399796904"
+    },
+    {
+        "STT": "32",
+        "Họ và Tên": "Lê Thị Minh",
+        "Tên khác": "4 Nhơn",
+        "Mã Số Điện": "31812",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "33",
+        "Họ và Tên": "Trương Hoàng Vinh",
+        "Tên khác": "2 Lập",
+        "Mã Số Điện": "40699",
+        "Số điện thoại": "0855610915"
+    },
+    {
+        "STT": "34",
+        "Họ và Tên": "Lê Tấn Quốc",
+        "Tên khác": "",
+        "Mã Số Điện": "23162",
+        "Số điện thoại": "0964971474"
+    },
+    {
+        "STT": "35",
+        "Họ và Tên": "Bùi Thị Bạc",
+        "Tên khác": "",
+        "Mã Số Điện": "31810",
+        "Số điện thoại": "0978949391"
+    },
+    {
+        "STT": "36",
+        "Họ và Tên": "Phạm Hữu Tình",
+        "Tên khác": "5 Tình",
+        "Mã Số Điện": "31834",
+        "Số điện thoại": "0866972330"
+    },
+    {
+        "STT": "37",
+        "Họ và Tên": "Phùng Thị Vân",
+        "Tên khác": "",
+        "Mã Số Điện": "06210",
+        "Số điện thoại": "0395958857"
+    },
+    {
+        "STT": "38",
+        "Họ và Tên": "Hồ Thị Huệ",
+        "Tên khác": "7 Đờn",
+        "Mã Số Điện": "31819",
+        "Số điện thoại": "0968432206"
+    },
+    {
+        "STT": "39",
+        "Họ và Tên": "Đặng Văn Sáng",
+        "Tên khác": "Út Sáng",
+        "Mã Số Điện": "31827",
+        "Số điện thoại": "0395958581"
+    },
+    {
+        "STT": "40",
+        "Họ và Tên": "Võ Văn Tờ",
+        "Tên khác": "4 Tợ",
+        "Mã Số Điện": "06201",
+        "Số điện thoại": "0971444763"
+    },
+    {
+        "STT": "41",
+        "Họ và Tên": "Phạm Văn Hải",
+        "Tên khác": "Hải 8 Tòng",
+        "Mã Số Điện": "06271",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "42",
+        "Họ và Tên": "Phan Thị Mừng",
+        "Tên khác": "",
+        "Mã Số Điện": "06181",
+        "Số điện thoại": "0944227656"
+    },
+    {
+        "STT": "43",
+        "Họ và Tên": "Võ Thanh Thái",
+        "Tên khác": "Đẹt",
+        "Mã Số Điện": "29740",
+        "Số điện thoại": "0862442558"
+    },
+    {
+        "STT": "44",
+        "Họ và Tên": "Phạm Văn Hoàng",
+        "Tên khác": "",
+        "Mã Số Điện": "31830",
+        "Số điện thoại": "0976118283"
+    },
+    {
+        "STT": "45",
+        "Họ và Tên": "Phạm Văn Thiết",
+        "Tên khác": "",
+        "Mã Số Điện": "31809",
+        "Số điện thoại": "0369607025"
+    },
+    {
+        "STT": "46",
+        "Họ và Tên": "Trương Văn Hương",
+        "Tên khác": "Văn Hường",
+        "Mã Số Điện": "36071",
+        "Số điện thoại": "0338649981"
+    },
+    {
+        "STT": "47",
+        "Họ và Tên": "Nguyễn Thành Tài",
+        "Tên khác": "Yến",
+        "Mã Số Điện": "21384",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "48",
+        "Họ và Tên": "Nguyễn Thành Long",
+        "Tên khác": "Cà Me",
+        "Mã Số Điện": "11337",
+        "Số điện thoại": "037412741"
+    },
+    {
+        "STT": "49",
+        "Họ và Tên": "Lý Thị Cơ",
+        "Tên khác": "3 Trạng",
+        "Mã Số Điện": "19425",
+        "Số điện thoại": "0356779659"
+    },
+    {
+        "STT": "50",
+        "Họ và Tên": "Võ Văn Tèo",
+        "Tên khác": "Tèo 4 Hoàng",
+        "Mã Số Điện": "06186",
+        "Số điện thoại": "0334832332"
+    },
+    {
+        "STT": "51",
+        "Họ và Tên": "Trần Thị Tuyết Sương",
+        "Tên khác": "Danh 5 Tình",
+        "Mã Số Điện": "31828",
+        "Số điện thoại": "0967046800"
+    },
+    {
+        "STT": "52",
+        "Họ và Tên": "Nguyễn Văn Đẹt",
+        "Tên khác": "8 Đẹt",
+        "Mã Số Điện": "46566",
+        "Số điện thoại": "0886504770"
+    },
+    {
+        "STT": "53",
+        "Họ và Tên": "Nguyễn Văn Chắp",
+        "Tên khác": "",
+        "Mã Số Điện": "06229",
+        "Số điện thoại": "0886504770"
+    },
+    {
+        "STT": "54",
+        "Họ và Tên": "Nguyễn Thị Kim Hà",
+        "Tên khác": "",
+        "Mã Số Điện": "27766",
+        "Số điện thoại": "0886504770"
+    },
+    {
+        "STT": "55",
+        "Họ và Tên": "Nguyễn Minh Diễn",
+        "Tên khác": "",
+        "Mã Số Điện": "32208",
+        "Số điện thoại": "0886504770"
+    },
+    {
+        "STT": "56",
+        "Họ và Tên": "Nguyễn Văn Đẹt",
+        "Tên khác": "",
+        "Mã Số Điện": "50029",
+        "Số điện thoại": "0886504770"
+    },
+    {
+        "STT": "57",
+        "Họ và Tên": "Nguyễn Minh Kinh",
+        "Tên khác": "",
+        "Mã Số Điện": "18142",
+        "Số điện thoại": "0988327802"
+    },
+    {
+        "STT": "58",
+        "Họ và Tên": "Đặng Văn Hùng",
+        "Tên khác": "5 Hùng",
+        "Mã Số Điện": "17906",
+        "Số điện thoại": "0933442695"
+    },
+    {
+        "STT": "59",
+        "Họ và Tên": "Đặng Văn Hùng",
+        "Tên khác": "Hạnh",
+        "Mã Số Điện": "35211",
+        "Số điện thoại": "0933442695"
+    },
+    {
+        "STT": "60",
+        "Họ và Tên": "Đặng Thị Mỹ Hạnh",
+        "Tên khác": "Hạnh",
+        "Mã Số Điện": "48272",
+        "Số điện thoại": "0933442695"
+    },
+    {
+        "STT": "61",
+        "Họ và Tên": "Lê Thị Kính",
+        "Tên khác": "Trung ấp 5",
+        "Mã Số Điện": "21990",
+        "Số điện thoại": "0987203305"
+    },
+    {
+        "STT": "62",
+        "Họ và Tên": "Triệu Quý Trung",
+        "Tên khác": "Trung ấp 5",
+        "Mã Số Điện": "45456",
+        "Số điện thoại": "0987203305"
+    },
+    {
+        "STT": "63",
+        "Họ và Tên": "Ngô Hoàng Sen",
+        "Tên khác": "",
+        "Mã Số Điện": "10985",
+        "Số điện thoại": "0374878652"
+    },
+    {
+        "STT": "64",
+        "Họ và Tên": "Trịnh Văn Xà",
+        "Tên khác": "",
+        "Mã Số Điện": "10986",
+        "Số điện thoại": "0374878652"
+    },
+    {
+        "STT": "65",
+        "Họ và Tên": "Ngô Hoàng Sen",
+        "Tên khác": "",
+        "Mã Số Điện": "37915",
+        "Số điện thoại": "0374878652"
+    },
+    {
+        "STT": "66",
+        "Họ và Tên": "Lê Văn Na",
+        "Tên khác": "4 Đực Na",
+        "Mã Số Điện": "11012",
+        "Số điện thoại": "0374878652"
+    },
+    {
+        "STT": "67",
+        "Họ và Tên": "Đỗ Văn Ro",
+        "Tên khác": "6 Rết",
+        "Mã Số Điện": "31817",
+        "Số điện thoại": "0366673930"
+    },
+    {
+        "STT": "68",
+        "Họ và Tên": "Nguyễn Hữu Hùng",
+        "Tên khác": "",
+        "Mã Số Điện": "06220",
+        "Số điện thoại": "0345234480"
+    },
+    {
+        "STT": "69",
+        "Họ và Tên": "Vỏ Văn Ngọt",
+        "Tên khác": "8 Ngọt",
+        "Mã Số Điện": "06203",
+        "Số điện thoại": "0785016522"
+    },
+    {
+        "STT": "70",
+        "Họ và Tên": "Huỳnh Văn Thắng",
+        "Tên khác": "Thu Tè",
+        "Mã Số Điện": "14709",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "71",
+        "Họ và Tên": "Trần Tấn Ren",
+        "Tên khác": "4 Ren",
+        "Mã Số Điện": "06225",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "72",
+        "Họ và Tên": "Đăng Văn Châu",
+        "Tên khác": "Châu Bò",
+        "Mã Số Điện": "06173",
+        "Số điện thoại": "0976732952"
+    },
+    {
+        "STT": "73",
+        "Họ và Tên": "Nguyễn Văn Định",
+        "Tên khác": "",
+        "Mã Số Điện": "23219",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "74",
+        "Họ và Tên": "Nguyễn Văn Mướt",
+        "Tên khác": "",
+        "Mã Số Điện": "06179",
+        "Số điện thoại": "0377139060"
+    },
+    {
+        "STT": "75",
+        "Họ và Tên": "Đặng Minh Luân",
+        "Tên khác": "5 Luân",
+        "Mã Số Điện": "31818",
+        "Số điện thoại": "0362734878"
+    },
+    {
+        "STT": "76",
+        "Họ và Tên": "Trần Thanh Tuấn",
+        "Tên khác": "",
+        "Mã Số Điện": "31829",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "77",
+        "Họ và Tên": "Nguyễn Thị Thiện",
+        "Tên khác": "Thẹn",
+        "Mã Số Điện": "26258",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "78",
+        "Họ và Tên": "Võ Thị Lệ Hằng",
+        "Tên khác": "",
+        "Mã Số Điện": "22301",
+        "Số điện thoại": "0775695453"
+    },
+    {
+        "STT": "79",
+        "Họ và Tên": "Nguyễn Văn Trường",
+        "Tên khác": "",
+        "Mã Số Điện": "47250",
+        "Số điện thoại": "0817843304"
+    },
+    {
+        "STT": "80",
+        "Họ và Tên": "Trần Văn Thiện",
+        "Tên khác": "Thiện Phượng",
+        "Mã Số Điện": "31814",
+        "Số điện thoại": "0384370605"
+    },
+    {
+        "STT": "81",
+        "Họ và Tên": "Lê Văn Tuấn",
+        "Tên khác": "7 Tuấn",
+        "Mã Số Điện": "06198",
+        "Số điện thoại": "0388040244"
+    },
+    {
+        "STT": "82",
+        "Họ và Tên": "Trần Ngọc Lệ",
+        "Tên khác": "",
+        "Mã Số Điện": "06223",
+        "Số điện thoại": "0798652842"
+    },
+    {
+        "STT": "83",
+        "Họ và Tên": "Phan Thành Nhân",
+        "Tên khác": "4 Ki",
+        "Mã Số Điện": "06177",
+        "Số điện thoại": "0937013954"
+    },
+    {
+        "STT": "84",
+        "Họ và Tên": "Đỗ Ngọc Răng",
+        "Tên khác": "9 Răng",
+        "Mã Số Điện": "31826",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "85",
+        "Họ và Tên": "Nguyễn Thành Tài",
+        "Tên khác": "Yến",
+        "Mã Số Điện": "21384",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "86",
+        "Họ và Tên": "Võ Văn Canh",
+        "Tên khác": "",
+        "Mã Số Điện": "27727",
+        "Số điện thoại": "0931861352"
+    },
+    {
+        "STT": "87",
+        "Họ và Tên": "Huỳnh Văn Lấn",
+        "Tên khác": "7 Lấn",
+        "Mã Số Điện": "06211",
+        "Số điện thoại": "0334100723"
+    },
+    {
+        "STT": "88",
+        "Họ và Tên": "Phan Văn Dũng",
+        "Tên khác": "Dũng Trai",
+        "Mã Số Điện": "14708",
+        "Số điện thoại": "0388763773"
+    },
+    {
+        "STT": "89",
+        "Họ và Tên": "Võ Công Dũng",
+        "Tên khác": "Tuyết Dũng",
+        "Mã Số Điện": "06174",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "90",
+        "Họ và Tên": "Nguyễn Minh Hoàng",
+        "Tên khác": "Út Hoàng Râu",
+        "Mã Số Điện": "28716",
+        "Số điện thoại": "0378062794"
+    },
+    {
+        "STT": "91",
+        "Họ và Tên": "Nguyễn Văn Lập",
+        "Tên khác": "Lập 3 Do",
+        "Mã Số Điện": "31807",
+        "Số điện thoại": "0974987448"
+    },
+    {
+        "STT": "92",
+        "Họ và Tên": "Đậu Xuân Quản",
+        "Tên khác": "",
+        "Mã Số Điện": "27516",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "93",
+        "Họ và Tên": "Trần Văn Nhung",
+        "Tên khác": "Út Nhung",
+        "Mã Số Điện": "41995",
+        "Số điện thoại": "0382707122"
+    },
+    {
+        "STT": "94",
+        "Họ và Tên": "Trần Văn Nhung",
+        "Tên khác": "Út Nhung",
+        "Mã Số Điện": "31805",
+        "Số điện thoại": "0382707122"
+    },
+    {
+        "STT": "95",
+        "Họ và Tên": "Ngô Văn Hùng",
+        "Tên khác": "",
+        "Mã Số Điện": "10998",
+        "Số điện thoại": "0986540719"
+    },
+    {
+        "STT": "96",
+        "Họ và Tên": "Kim Vỉnh Quới",
+        "Tên khác": "",
+        "Mã Số Điện": "50083",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "97",
+        "Họ và Tên": "Kim Thị Trà My",
+        "Tên khác": "",
+        "Mã Số Điện": "50195",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "98",
+        "Họ và Tên": "Nguyễn Thị Kim Hương",
+        "Tên khác": "4 Hương",
+        "Mã Số Điện": "31820",
+        "Số điện thoại": "0976332973"
+    },
+    {
+        "STT": "99",
+        "Họ và Tên": "Nguyễn Văn Dần",
+        "Tên khác": "",
+        "Mã Số Điện": "06175",
+        "Số điện thoại": "0773662223"
+    },
+    {
+        "STT": "100",
+        "Họ và Tên": "Nguyễn Thành Nhân",
+        "Tên khác": "Hưởng 4 Căn",
+        "Mã Số Điện": "14548",
+        "Số điện thoại": "0836886382"
+    },
+    {
+        "STT": "101",
+        "Họ và Tên": "",
+        "Tên khác": "5 Nở",
+        "Mã Số Điện": "29509",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "102",
+        "Họ và Tên": "",
+        "Tên khác": "5 Nở",
+        "Mã Số Điện": "33171",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "103",
+        "Họ và Tên": "",
+        "Tên khác": "5 Nở",
+        "Mã Số Điện": "06184",
+        "Số điện thoại": ""
+    },
+    {
+        "STT": "104",
+        "Họ và Tên": "Nguyễn Minh Nguyên",
+        "Tên khác": "Thân 4 Nhơn",
+        "Mã Số Điện": "45651",
+        "Số điện thoại": "0373758395"
+    },
+    {
+        "STT": "105",
+        "Họ và Tên": "Võ Thị Liễu",
+        "Tên khác": "",
+        "Mã Số Điện": "23163",
+        "Số điện thoại": "0337878952"
+    },
+    {
+        "STT": "106",
+        "Họ và Tên": "Trịnh Văn Phương",
+        "Tên khác": "",
+        "Mã Số Điện": "10989",
+        "Số điện thoại": "0916478083"
+    },
+    {
+        "STT": "107",
+        "Họ và Tên": "Nguyễn Thị Thu Thảo",
+        "Tên khác": "",
+        "Mã Số Điện": "50681",
+        "Số điện thoại": "0986455344"
+    }
+]
+}
